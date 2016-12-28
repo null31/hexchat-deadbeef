289bc1df6ced588c1ba8639bfb2b12c3def56a80
null31 <null@rwx.moe>
2016-12-28 14:09:29 -0200

    Changed versions of module and DeaDBeeF. Changed command in Xchat/Hexchat to /np to show the info.

--- XChat-DeaDBeeF-v2-py3.py
+++ XChat-DeaDBeeF-v2-py3.py
@@ -10,14 +10,14 @@
 #  This particular version is simplified and can only output
 #  the results of /np command in like so:
 #
-#  * mw is listening to Artist - (Album) - Track [XXbit / YYYYkbps / ZZZZZZHz]
+#  * nickname is listening to: Artist - Album - Track | Playback / Length | Codec | yyyy kbps | zzzzzHZ
 #
 
 __module_name__ = "XChat-DeaDBeeF"
-__module_author__ = "iceTwy / mwgg"
+__module_author__ = "iceTwy / mwgg / null31"
 __module_description__ = "DeaDBeeF integration in XChat and HexChat."
-__module_version__ = "1.0"
-__module_deadbeef_version__ = "0.5.6"
+__module_version__ = "2.0"
+__module_deadbeef_version__ = "0.7.0"
 
 ############################
 import xchat
@@ -29,7 +29,7 @@ from time import sleep
 
 def deadbeef_current_track(word, word_eol, userdata):
         read_track = subprocess.check_output('/usr/bin/deadbeef --nowplaying-tf "%artist% - %album% - %title% | %playback_time% / %length% | %codec% | %bitrate% kbps | %samplerate%Hz"',shell=True).decode("utf-8")
-        xchat.command("me is listening to " + read_track)
+        xchat.command("me is listening to: " + read_track)
         return xchat.EAT_ALL
 
 def unload(userdata):
@@ -40,4 +40,4 @@ if __name__ == '__main__':
         print("XChat-DeaDBeeF %s loaded successfully! - by %s" % (__module_version__,__module_author__))
 
 #Display the current track
-        xchat.hook_command('tellnp',deadbeef_current_track)
+        xchat.hook_command('np',deadbeef_current_track)
