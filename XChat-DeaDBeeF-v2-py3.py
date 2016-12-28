e4fc0214ebabceb0711de3fdc4f2209513c5f92b
null31 <null@rwx.moe>
2016-12-28 13:52:38 -0200

    Updated command --nowplaying to --nowplaying-tf with your syntax of args

--- XChat-DeaDBeeF-v2-py3.py
+++ XChat-DeaDBeeF-v2-py3.py
@@ -3,15 +3,15 @@
 #  XChat-DeaDBeeF  -  XChat/HexChat script for DeaDBeeF integration
 #  Python 3 version
 #
-#  Unless indicated otherwise, files from the XChat-DeaDBeeF project 
+#  Unless indicated otherwise, files from the XChat-DeaDBeeF project
 #  are licensed under the WTFPL version 2. Full license information
 #  for the WTFPL version 2 can be found in the LICENSE.txt file.
 #
 #  This particular version is simplified and can only output
 #  the results of /np command in like so:
-#  
+#
 #  * mw is listening to Artist - (Album) - Track [XXbit / YYYYkbps / ZZZZZZHz]
-#  
+#
 
 __module_name__ = "XChat-DeaDBeeF"
 __module_author__ = "iceTwy / mwgg"
@@ -20,15 +20,15 @@ __module_version__ = "1.0"
 __module_deadbeef_version__ = "0.5.6"
 
 ############################
-import xchat              
+import xchat
 import subprocess
 
-from threading import Thread 
-from time import sleep      
+from threading import Thread
+from time import sleep
 ############################
-	
+
 def deadbeef_current_track(word, word_eol, userdata):
-        read_track = subprocess.check_output('/usr/bin/deadbeef --nowplaying "%a - (%b) - %t [%@:BPS@bit / %@:BITRATE@kbps / %@:SAMPLERATE@Hz]"',shell=True).decode("utf-8")
+        read_track = subprocess.check_output('/usr/bin/deadbeef --nowplaying-tf "%artist% - %album% - %title% | %playback_time% / %length% | %codec% | %bitrate% | %samplerate% | $info(__bitspersample)"',shell=True).decode("utf-8")
         xchat.command("me is listening to " + read_track)
         return xchat.EAT_ALL
 
